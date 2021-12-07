3c564167e9f2b920580099e0818e52f7c1e4890e
Evair Vargas <evairvargas7@gmail.com>
2021-12-06 21:45:03 -0300

    Trabalho N2

--- AppN2/app/src/main/java/com/vargas/appn2/MainActivity.java
+++ AppN2/app/src/main/java/com/vargas/appn2/MainActivity.java
@@ -8,6 +8,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +17,7 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText nome, altura, peso;
+    EditText nome, altura, peso;
     Button btnCalcular;
 
     @SuppressLint("WrongViewCast")
@@ -34,24 +35,31 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                String editNome = nome.getText().toString();
-                String editAltura = altura.getText().toString();
-                String editPeso = peso.getText().toString();
-                if(editNome.isEmpty() || editAltura.isEmpty() || editPeso.isEmpty()){
-                    Snackbar.make(view,"Preencha todos os dados para calcular",Snackbar.LENGTH_LONG).show();
-                }
-
                 resultadoIMC();
+
             }
         });
     }
 
     public void resultadoIMC(){
-        Intent intent = new Intent(this, ResultadoIMC.class); // quando clicar e iniciar esse método, vai mandar pra tela de ResultadoIMC
+
+        if(TextUtils.isEmpty(nome.getText().toString())){
+            nome.setError("Preencha o campo com seu nome.");
+            return;
+        }
+        if(TextUtils.isEmpty(altura.getText().toString())){
+            altura.setError("Preencha o campo com a sua altura.");
+            return;
+        }
+        if(TextUtils.isEmpty(peso.getText().toString())){
+            peso.setError("Preencha o campo com seu peso.");
+            return;
+        }
+
+        Intent intent = new Intent(MainActivity.this, ResultadoIMC.class); // quando clicar e iniciar esse método, vai mandar pra tela de ResultadoIMC
         intent.putExtra("nome", nome.getText().toString()); // vai mandar pra tela ResultadoIMC o nome coletado nesta tela atual por parâmetro
         intent.putExtra("altura", altura.getText().toString()); // conforme a linha acima, vai mandar também a altura via parâmetro
         intent.putExtra("peso", peso.getText().toString()); // o mesmo vale para o peso
-
         startActivity(intent); // quando clicar em calcular, ele vai iniciar a intent do ResultadoIMC
     }
 
--- AppN2/app/src/main/java/com/vargas/appn2/ResultadoIMC.java
+++ AppN2/app/src/main/java/com/vargas/appn2/ResultadoIMC.java
@@ -11,8 +11,8 @@ import org.w3c.dom.Text;
 public class ResultadoIMC extends AppCompatActivity {
 
     TextView resultado;
-    String strNome;
-    float floatAltura, floatPeso, floatResultado;
+    String strNome, strResult;
+    Float floatAltura, floatPeso, floatResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,40 +20,41 @@ public class ResultadoIMC extends AppCompatActivity {
         setContentView(R.layout.activity_resultado_imc);
 
         resultado = findViewById(R.id.txtResultado); // parametro para buscar o resultado que vai aparecer na segunda tela
+
         Intent intent = getIntent(); // criei uma intent chamada intent para pegar o que estava na tela anterior
+
         strNome = intent.getStringExtra("nome"); // usei o getStringExtra para PEGAR os dados da variável strNome
         floatAltura = Float.parseFloat(intent.getStringExtra("altura")); // usei o getIntExtra para PEGAR os dados da variável intAltura
         floatPeso = Float.parseFloat(intent.getStringExtra("peso")); // usei o getIntExtra para PEGAR os dados da variável intPeso
 
         floatResultado = floatPeso / (floatAltura * floatAltura); // resultado do cálculo do IMC
 
-        String strResult = "Olá " + strNome + "!";
-        strResult = strResult = "\n" + "O resultado do seu IMC é " + floatResultado;
-        //peguei a variável 'resultado' que criei para conseguir buscar o textView da segunda tela através da linha 22 para setar o texto que deve ser mostrado
-        resultado.setText(strResult);
+        strResult = "Olá, " + strNome + "!";
+        strResult = strResult + "\n" + "O resultado do seu IMC é " + floatResultado.toString();
 
                 if (floatResultado < 17) {
-                    resultado.setText("\n" + "Você está muito abaixo do peso.");
+                    strResult = strResult + "\n" + "Você está muito abaixo do peso.";
                 }
                 else if(floatResultado < 18.49){
-                    resultado.setText("\n" + "Você está abaixo do peso normal.");
+                    strResult = strResult + "\n" + "Você está abaixo do peso.";
                 }
                 else if(floatResultado < 24.99){
-                    resultado.setText("\n" + "Você está no peso normal.");
+                    strResult = strResult + "\n" + "Você está no peso normal.";
                 }
                 else if(floatResultado < 29.99){
-                    resultado.setText("\n" + "Você está acima do peso normal.");
+                    strResult = strResult + "\n" + "Você está acima do peso.";
                 }
                 else if(floatResultado < 34.99){
-                    resultado.setText("\n" + "Você está na faixa de obesidade 1.");
+                    strResult = strResult + "\n" + "Você está na faixa de obesidade I.";
                 }
                 else if(floatResultado < 39.99){
-                    resultado.setText("\n" + "Você está na faixa de obesidade 2 (severa).");
+                    strResult = strResult + "\n" + "Você está na faixa de obesidade II (severa).";
                 }
                 else {
-                    resultado.setText("\n" + "Você está na faixa de obesidade 3 (mórbida).");
+                    strResult = strResult + "\n" + "Você está na faixa de obesidade III (mórbida).";
                 }
 
+                resultado.setText(strResult);
     }
 }
 
